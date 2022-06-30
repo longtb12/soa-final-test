932b1504ab37654cd6c77ee154639a33ee291905
Hai Long <longvh@tuivietnam.com>
2022-06-30 23:04:48 +0700

    update

--- ReminderService/index.js
+++ ReminderService/index.js
@@ -1,19 +1,27 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 5555;
-const {sendMail} = require('./service/index')
+const { createNoteRemind, checkStatus } = require('./service/index')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-app.post("/send-email", async (req, res) => {
-  const data = await sendMail(req)
+
+app.post("/reminder", async (req, res) => {
+  const data = await createNoteRemind(req)
   res.json(data);
 });
+
+app.post("/status", async (req, res) => {
+  const data = await checkStatus(req)
+  res.json(data);
+});
+
 app.listen(process.env.PORT||port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- ReminderService/service/index.js
+++ ReminderService/service/index.js
@@ -1,74 +1,71 @@
-require("dotenv").config();
-const Joi = require('@hapi/joi');
 const moment = require('moment');
+const apiAdapter = require('./apiAdapter')
+const api = apiAdapter('http://localhost:4444')
+var global = []
 
-function remindFunction(request){
-    createNoteRemind(request)
-    return render(request, 'home.html')
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-    
 
+async function createNoteRemind(request) {
+    var { body } = request;
+    var returnDate = moment(body.returnDate);
+    var isAlert = false;
 
-function myThread {
-    self.deadline = datetime.strptime(self.date, '%Y-%m-%dT%H:%M')
-        print(self.deadline)
-        # print(datetime.now())
-        isAlert5min = False
-        alert = "5mins"
-        value1 = datetime.now() + timedelta(hours=7) 
+    global.push({
+        taskId: body.task_id,
+        status: 'Wait',
+        message: 'Wait'
+    })
 
-        # date_time = value.strftime("%Y-%m-%d")
-        print(value1)
+    while (!isAlert) {
+        var now = new Date();
+        var diffInMinutes = returnDate.diff(now, 'minutes');
+        var diffInSeconds = returnDate.diff(now, 'seconds') - 60*diffInMinutes;
 
-        while True:
-            value = datetime.now() + timedelta(hours=7) 
+        var task = global.find(x => x.taskId == body.task_id);
+        task.message = 'Wait in ' + diffInMinutes + 'minutes and '+ diffInSeconds + 'seconds';
 
-            # date_time = value.strftime("%Y-%m-%d")
-            # print(value)
-            minutes_diff = (self.deadline - value).total_seconds() / 60.0
-            isAlert = False
-            # print(minutes_diff)
+        if (diffInMinutes <= 5) {
+            isAlert = true;
+            var reqEmail = {
+                task_id: body.task_id,
+                to: body.borrow_email,
+                subject: 'Borrow book',
+                text: 'ok'
+            }
+            resp = await sendMail(reqEmail);
+            task.message = 'Sent email!';
+            task.status = 'Done!';
+        }
+        await sleep(1000);
+    }
 
-            if minutes_diff< 5 and isAlert5min == False:
-                alert = "5mins"
-                isAlert5min = True
-                isAlert = True
-            elif minutes_diff <= 1 and isAlert5min == True:
-                alert = "0mins"
-                isAlert = True
-            if isAlert == True:
-                context = {
-                    "nameTask" : self.taskname,
-                    "userCreate" : self.user,
-                    "subject" : self.subject,
-                    "message" : self.message,
-                    "email" : self.email,
-                    "deadline" : self.date,
-                    "alert": alert
-                    }
-                print("oke")
-                isAlert = False
-                self.sendmail(self.request, context)
-                if alert == "0mins":
-                    return
-    def __init__(self, user, taskname,subject, date, email, message, request):
-        # print(datetime.today().date)
-        threading.Thread.__init__(self)
-        self.user = user
-        self.subject = subject
-        self.taskname = taskname
-        self.date = date
-        self.email = email
-        self.message = message
-        self.request = request
-
-    def run(self):
-        
-    def sendmail(self, request, context):
-        import json
-        resp = requests.post('http://127.0.0.1:9999/api/v1/sendmail', data = json.dumps(context), headers={"Content-Type":"application/json"})
-        print(resp.text)
-        return resp
-        # return HttpResponse('Hello, async world!')
-        
+    return true;
 }
+
+async function sendMail(request) {
+    api.post('/email', request).then(resp => {
+        return resp.data;
+    })
+        .catch(err => {
+            console.log(err.message)
+            return null;
+        })
+}
+
+async function checkStatus(params) {
+    const {body} = params
+    const task = global.find(x => x.taskId == body.task_id);
+    if(task == undefined || task == null) {
+        return {
+            status: 'No'
+        };
+    }
+    return {
+        status: task.status,
+        message: task.message
+    };
+}
+
+module.exports = { createNoteRemind, checkStatus }
